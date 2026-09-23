76e6e87e6cc40b55faab4b47f90618aacafdd24d
Maxim-1-cchgeu <a190lmax@yandex.ru>
2026-09-23 15:20:21 +0300

    Update source.c

--- source.c
+++ source.c
@@ -1,30 +1,26 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<locale.h>
+#include<stdlib.h>
 
-void f2()
-{
-    int a = 11;
-    int b = 3;
-    printf("(float) a / b = %f\n", (float)a / b);
-    printf("(double) a / b = %f\n", (double)a / b);
-}
-
-void f3()
-{
-    int n;
-    puts("Введите целое трёхзначное число:");
-    scanf("%3d", &n);
-    int first_digit = n / 100;
-    int last_digit = n % 10;
-    int sum = first_digit + last_digit;
-    printf("Последняя цифра %d, первая - %d, сумма цифра %d", last_digit, first_digit, sum);
-}
+void f1();
+void f2();
+void f3();
 
 void main()
 {
     setlocale(LC_CTYPE, "RUS.UTF-8");
 
+    f1();
+    puts("");
+    f2();
+    puts("");
+    f3();
+    system("pause");
+}
+
+void f1()
+{
     char c;
     int i;
     float f;
@@ -38,18 +34,33 @@ void main()
     puts("Введите целое число:");
     scanf("%d", &i);
     printf("Введено целое число %d\n", i);
-    printf("1 / %d = %d\n\n", i, 1/i);
+    printf("1 / %d = %f\n\n", i, 1.f/i);
     puts("Введите вещественное число:");
     scanf("%f", &f);
     printf("Введено вещественное число %f\n", f);
     printf("Целая часть: %d\n", (int)f);
-    printf("Дробная часть: %5.5g\n\n", f - (int)f);
-    puts("Введите вещественное число:");
-    scanf("%e", &d);
-    printf("Введено вещественное число %e\n", d);
-    puts("");
-    f2();
-    puts("");
-    f3();
-    system("pause");
+    printf("Дробная часть: %6.6g\n\n", f - (int)f);
+    puts("Введите вещественное число двойной точности:");
+    scanf("%le", &d);
+    printf("Введено вещественное число двойной точности %le\n", d);
+}
+
+void f2()
+{
+    int a = 11;
+    int b = 3;
+    printf("a = %d, b = %d\n", a, b);
+    printf("(float) a / b = %f\n", (float)a / b);
+    printf("(double) a / b = %lf\n", (double)a / b);
+}
+
+void f3()
+{
+    int n;
+    puts("Введите целое трёхзначное число:");
+    scanf("%3d", &n);
+    int first_digit = n / 100;
+    int last_digit = n % 10;
+    int sum = first_digit + last_digit;
+    printf("Последняя цифра %d, первая - %d, сумма цифр - %d\n", last_digit, first_digit, sum);
 }
